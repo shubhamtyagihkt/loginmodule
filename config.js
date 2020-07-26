3d5aed9be824ea1c37a3aa1ff1387fbd90debf15
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-07-26 09:51:48 +0530

    first commit login module

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"dbname": process.env.DBNAME,
+	"dbhost": process.env.DBHOST,
+	"dbuser": process.env.DBUSER,
+	"dbpwd": process.env.DBPWD,
+
+	"jwtsecret": process.env.JWTSEC,
+	"jwtissuer": process.env.JWTUSR,
+	"jwtexpiry": process.env.JWTEXP
+}
